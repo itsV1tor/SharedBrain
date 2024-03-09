7633aff785ca5b305d9a9514d07a5fc727b1fb2f
H4KV <vitor.sannctorum@hotmail.com>
2024-03-09 00:02:49 -0300

    Update: Add get method & List configured

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,27 +1,58 @@
+import { useState, useEffect } from 'react';
 import { FaRegCalendarAlt } from 'react-icons/fa';
+import axios from 'axios';
 
 export function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          'https://shared-brain-api.onrender.com/posts',
+        );
+
+        setPosts(response.data);
+      } catch (error) {
+        setError('Error when searching for posts!');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  const generateRandomUserId = () => {
+    const randomUserId = Math.random().toString(36).substring(2, 12);
+    return randomUserId;
+  }
+
+  console.log(generateRandomUserId())
+
   return (
     <div className="mt-4 mb-40">
       <h2 className="uppercase font-bold text-2xl mb-4">Posts</h2>
+      {loading && <p>Loading posts...</p>}
 
-      <div className="p-2 border-2">
-        <div className="flex justify-between mb-2">
-          <span>User: 1242a6cc8904</span>
-          <div className="flex items-center gap-2">
-            <FaRegCalendarAlt />
-            <p>03/08/2022</p>
+      {error && <p>{error}</p>}
+
+      {posts.map((post) => (
+        <div key={post.id} className="p-2 border-2">
+          <div className="flex justify-between mb-2">
+            <span className='uppercase'>User: { generateRandomUserId() }</span>
+            <div className="flex items-center gap-2">
+              <FaRegCalendarAlt />
+              <p>{post.created_at}</p>
+            </div>
           </div>
-        </div>
 
-        <h3 className="text-lg border-b-2 pb-2">title</h3>
-        <p className='mt-2'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo,
-          reprehenderit distinctio. Maiores magni, sed veritatis nemo assumenda
-          repudiandae ipsam similique, dolor a recusandae alias, explicabo
-          officiis perferendis vero? Beatae, dolor.
-        </p>
-      </div>
+          <h3 className="text-lg border-b-2 pb-2">{post.title}</h3>
+          <p className="mt-2">{post.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
